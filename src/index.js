193ec8bd94015c06f659fc3865754ebab2e9c775
Motkova Kateryna <sirenkoekaterina90@gmail.com>
2022-12-24 11:36:33 +0200

    HW 11

--- src/index.js
+++ src/index.js
@@ -1,52 +1,58 @@
+import { Notify } from 'notiflix';
+import '../node_modules/modern-normalize/modern-normalize.css';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import './css/styles.css';
-import Notiflix from 'notiflix';
-import { fetchPictures } from './js/fetchPictures.js';
-// import SimpleLightbox from 'simplelightbox';
-// const DEBOUNCE_DELAY = 300;
+import SimpleLightbox from 'simplelightbox';
+import { fetchImage } from './js/fetchPictures';
+
 const refs = {
   form: document.querySelector('.search-form'),
-  galleryBox: document.querySelector('.gallery'),
-  loadmoreBtn: document.querySelector('.load-more'),
+  gallery: document.querySelector('.gallery'),
+  searchBtn: document.querySelector('.search-btn'),
+  body: document.body,
+  sentinel: document.querySelector('.sentinel'),
+  spinner: document.querySelector('.spinner-border'),
 };
 
 let _page = 1;
 let _per_page = 40;
 let query = '';
 
-refs.form.addEventListener('submit', onImageSearch);
+refs.form.addEventListener('submit', handleSubmit);
 
-//----
-// async function handleSubmit(e) {
-//   e.preventDefault();
-//   observer.unobserve(refs.sentinel);
-//   const form = e.currentTarget;
-//   const searchQuery = form.elements.searchQuery.value.trim();
-//   if (searchQuery === '') return;
-//   if (searchQuery !== query) {
-//     refs.gallery.innerHTML = '';
-//     query = searchQuery;
-//     _page = 1;
-//   }
-//----
+refs.spinner.classList.add('js-hidden');
 
-async function onImageSearch(e) {
+const options = {
+  rootMargin: '150px',
+  threshold: 1.0,
+};
+
+const observer = new IntersectionObserver(onEntry, options);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  scrollZoom: false,
+});
+async function handleSubmit(e) {
   e.preventDefault();
+  observer.unobserve(refs.sentinel);
   const form = e.currentTarget;
-
   const inputValue = form.elements.searchQuery.value.trim();
-  //   console.log(inputValue);
+
   if (inputValue === '') {
-    Notiflix.Notify.info(
+    return Notify.info(
       'Enter in the field the words that describe the picture you want to find'
     );
   }
+
   if (inputValue !== query) {
-    refs.galleryBox.innerHTML = '';
+    refs.gallery.innerHTML = '';
     query = inputValue;
     _page = 1;
   }
 
-  await fetchPictures(query, _page, _per_page)
+  await fetchImage(query, _page, _per_page)
     .then(data => {
       if (data.hits.length === 0) {
         Notify.failure(
@@ -54,16 +60,74 @@ async function onImageSearch(e) {
         );
       } else {
         Notify.success(`Hooray! We found ${data.totalHits} images.`);
-        renderPicturesList(data.hits);
-        console.log(data.hits);
+        render(data.hits);
+        smoothScroll();
+        observer.observe(refs.sentinel);
       }
       return data;
     })
     .catch(err => err.message)
-    .finally(console.log(inputValue));
+    .finally(() => refs.spinner.classList.add('js-hidden'));
+
+  await lightbox.refresh();
+  await lightbox.on('shown.simplelightbox', function () {
+    refs.body.classList.add('disable-scroll');
+  });
+  await lightbox.on('closed.simplelightbox', function () {
+    refs.body.classList.remove('disable-scroll');
+  });
 }
 
-const createPicture = ({
+function onEntry(entries) {
+  entries.forEach(async entry => {
+    if (entry.isIntersecting && query !== '') {
+      _page += 1;
+      observer.observe(refs.sentinel);
+      refs.spinner.classList.remove('js-hidden');
+      await fetchImage(query, _page, _per_page)
+        .then(data => {
+          render(data.hits);
+          smoothScroll();
+          return data;
+        })
+        .then(data => {
+          const totalPage = data.totalHits / _per_page;
+          if (_page >= totalPage) {
+            Notify.info(
+              "We're sorry, but you've reached the end of search results."
+            );
+            observer.unobserve(refs.sentinel);
+            return;
+          }
+        })
+        .catch(err => err.message)
+        .finally(() => refs.spinner.classList.add('js-hidden'));
+      await lightbox.refresh();
+      await lightbox.on('shown.simplelightbox', () => {
+        refs.body.classList.add('disable-scroll');
+      });
+      await lightbox.on('closed.simplelightbox', () => {
+        refs.body.classList.remove('disable-scroll');
+      });
+    }
+  });
+}
+
+function render(items) {
+  const markup = items.map(getItemsTemplate).join('');
+  return refs.gallery.insertAdjacentHTML('beforeend', markup);
+}
+
+async function smoothScroll() {
+  const { height: cardHeight } =
+    refs.gallery.firstElementChild.getBoundingClientRect();
+
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
+}
+const getItemsTemplate = ({
   webformatURL,
   largeImageURL,
   tags,
@@ -91,8 +155,3 @@ const createPicture = ({
 </div>
 </a>`;
 };
-
-function renderPicturesList(items) {
-  const markup = items.map(createPicture).join('');
-  return refs.galleryBox.insertAdjacentHTML('beforeend', markup);
-}
--- src/js/fetchPictures.js
+++ src/js/fetchPictures.js
@@ -1,17 +1,17 @@
-import axios from 'axios';
-// const { default: axios } = require('axios');
+const { default: axios } = require('axios');
 
-const BASE_URL = 'https://pixabay.com/api/';
-const MY_KEY = '32149737-e90c4fbf5b87dac3ddee3c5b3';
 const searchParams = {
   image_type: 'photo',
   orientation: 'horizontal',
   safesearch: true,
 };
 
-export async function fetchPictures(query, page, per_page) {
-  const data = await axios.get(
-    `${BASE_URL}?key=${MY_KEY}&q=${query}&page=${page}&per_page=${per_page}`,
+const BASE_URL = 'https://pixabay.com/api/';
+const KEY_SUCSESS = '30821762-b8f2171742f26b4a3279c46a9';
+
+export async function fetchImage(query, page, per_page) {
+  const { data } = await axios.get(
+    `${BASE_URL}?key=${KEY_SUCSESS}&q=${query}&page=${page}&per_page=${per_page}`,
     searchParams
   );
   return data;
